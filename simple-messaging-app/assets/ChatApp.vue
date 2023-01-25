7d42ae72a3d9d759a169701e24d57d7a338b6eb9
Dominykas-V <dominykas.venskunas@gmail.com>
2023-01-25 17:02:04 +0200

    init

---
+++ simple-messaging-app/assets/ChatApp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>VUE TEST</h2>
+        <button @click="counter++">COUNTER: {{ counter }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
